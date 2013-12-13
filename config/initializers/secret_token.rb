e3d5f55f207c963a4b082d86e88935f2e0e47397
mintotsai <mintotsai@gmail.com>
2013-12-13 15:46:59 -0600

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+JazDev::Application.config.secret_token = '467905519448dce2fce67e3e835a432d9bbcfe4e1ed5fb8828ff3bfa388f6e6388ef8704458637349cbb6980f78397be69f14b0dbea65b9098a4681d6c0c32e0'
